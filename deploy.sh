fe9ace0b894fe55be513465e6bfc58433300cc1f
kif11 <kovalewskiy@gmail.com>
2023-07-03 12:10:35 -0400

    Do not sync Snay stories

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,12 @@ ssh -t codercat "sudo systemctl stop codercat-gallery.service"
 scp bin/gallery codercat:~/gallery/gallery
 
 # Sync assets
-rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r ./public/media/ codercat:~/gallery/public/media
-rsync -avu -p ./public/gallery/ codercat:~/gallery/public/gallery
-rsync -avu -p ./pages/ codercat:~/gallery/pages
+rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r ./public/media/kif/ codercat:~/gallery/public/media/kif
+rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --exclude 'story_*' ./public/media/snay/ codercat:~/gallery/public/media/snay
+rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r ./public/gallery/ codercat:~/gallery/public/gallery
+rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r ./public/*.css codercat:~/gallery/public
+
+rsync -avu --delete -p ./pages/ codercat:~/gallery/pages
 
 # Restart service
 ssh -t codercat "sudo systemctl start codercat-gallery.service"

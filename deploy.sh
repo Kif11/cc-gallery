c0e9ce726d013c89576946ed5ed42ed6e49049e7
kif11 <kovalewskiy@gmail.com>
2025-04-14 23:17:13 -0600

    Delay image load on scroll

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,9 @@ scp bin/gallery codercat:~/gallery/gallery
 # Sync assets
 # rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r ./public/media/kif/ codercat:~/gallery/public/media/kif
 # rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --exclude 'story_*' ./public/media/snay/ codercat:~/gallery/public/media/snay
-# rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r ./public/gallery/ codercat:~/gallery/public/gallery
-# rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r ./public/*.css codercat:~/gallery/public
+
+rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r ./public/gallery/ codercat:~/gallery/public/gallery
+rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r ./public/*.css codercat:~/gallery/public
 
 rsync -avu --delete -p ./pages/ codercat:~/gallery/pages
 

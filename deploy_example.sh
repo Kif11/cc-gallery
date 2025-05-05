5b08fb62e1d373b557b241c894ae6d5a8ba6d6c4
kif11 <kovalewskiy@gmail.com>
2025-05-05 23:10:36 +0900

    Less downtime deploy script

--- deploy_example.sh
+++ deploy_example.sh
@@ -1,17 +1,18 @@
+# (Optional) Sync images and video files if using local gallery
+# rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r ./assets/media/kif/ user@server:~/gallery/assets/media/
+
 # Make sure we are doing clean build
 rm -rf bin
 
 # Build for linux amd64
 GOOS=linux GOARCH=amd64 go build -o bin/gallery server.go
 
-# Stop remote service
-ssh -t user@server "sudo systemctl stop ccgallery.service"
-
 # Copy server binary to remote host
-scp bin/gallery user@server:~/bin/gallery
+scp bin/gallery codercat:~/gallery/gallery.new
 
-# (Optional) Sync images and video files if using local gallery
-# rsync -avu -p --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r ./assets/media/kif/ user@server:~/gallery/assets/media/
-
-# Restart service
-ssh -t user@server "sudo systemctl start ccgallery.service"
+# Swap to a new binary and restart the service
+ssh -t codercat "
+mv ~/gallery/gallery.new ~/gallery/gallery
+chmod +x ~/gallery/gallery
+sudo systemctl restart codercat-gallery.service
+"

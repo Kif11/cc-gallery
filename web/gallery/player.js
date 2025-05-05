b5191485d665ea3c8e9cd164329f105e4bf6f0c5
kif11 <kovalewskiy@gmail.com>
2025-05-05 18:53:36 +0900

    Better style for player navigation buttons

--- web/gallery/player.js
+++ web/gallery/player.js
@@ -1,8 +1,22 @@
 const videoEl = document.querySelector("video");
+const nextLink = document.querySelector(".nav-next");
+const prevLink = document.querySelector(".nav-prev");
 
 // Set player hotkeys
 document.addEventListener("keydown", (e) => {
     switch (e.key) {
+        case "ArrowLeft":
+            case "a":
+                if (prevLink) {
+                    prevLink.click();
+                }
+                break;
+            case "ArrowRight":
+            case "d":
+                if (nextLink) {
+                    nextLink.click();
+                }
+                break;
         case "m":
             videoEl.muted = !videoEl.muted;
             break;
--- web/global.js
+++ web/global.js
@@ -1,22 +1,8 @@
-const nextLink = document.querySelector(".next");
-const prevLink = document.querySelector(".prev");
-const backLink = document.querySelector(".back");
+const backLink = document.querySelector(".nav-back");
 
 // Set global hotkeys
 document.addEventListener("keydown", (e) => {
     switch (e.key) {
-        case "ArrowLeft":
-        case "a":
-            if (prevLink) {
-                prevLink.click();
-            }
-            break;
-        case "ArrowRight":
-        case "d":
-            if (nextLink) {
-                nextLink.click();
-            }
-            break;
         case "Escape":
         case "ArrowUp":
         case "w":
